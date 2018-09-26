d237b641209f5e0fdbc3ffb8be27cb75cf4c8d2f
집요정 <dongb94@gmail.com>
2018-09-26 18:26:04 +0900

    Add 2 Question of BeakJoon algorithm level 1 IO

---
+++ beak/level1/ROKA.java
@@ -0,0 +1,13 @@
+package 백준1단계_입출력;
+
+import java.io.*;
+public class ROKA {
+
+	public static void main(String[] args) throws IOException{
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		for(int i=0; i<2; i++) bw.write("강한친구 대한육군\n");
+		bw.flush();
+
+	}
+
+}
---
+++ beak/level1/dog.java
@@ -0,0 +1,16 @@
+package 백준1단계_입출력;
+import java.io.*;
+public class dog {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write("|\\_/|\n");
+		bw.write("|q p|   /}\n");
+		bw.write("( 0 )\"\"\"\\\n");
+		bw.write("|\"^\"`    |\n");
+		bw.write("||_/=\\\\__|\n");
+		bw.flush();
+	}
+
+}
