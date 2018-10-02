e716dfe18c80fc09bdf38c95adffc73a23ff59e8
집요정 <dongb94@gmail.com>
2018-10-02 22:59:31 +0900

    Add NumberSorting

---
+++ beak/level9/NumberSorting.java
@@ -0,0 +1,23 @@
+package beakJoon_Level9_Sort;
+
+import java.io.*;
+import java.util.*;
+
+public class NumberSorting {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		for(int i=0; i<N; i++) {
+			list.add(Integer.parseInt(br.readLine()));
+		}
+		Collections.sort(list);
+		
+		for(int i:list) bw.write(i+"\n");
+		bw.flush();
+	}
+
+}
