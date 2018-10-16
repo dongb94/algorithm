8d9d6ff82500d0a6af9aa6d131f851ac97eb63ac
DONGGEON BYEON <dongb94@gmail.com>
2018-10-16 20:00:11 +0900

    Add LCM

---
+++ beak/level17/LCM.java
@@ -0,0 +1,33 @@
+/* Q
+ * 두 자연수 A와 B에 대해서, A의 배수이면서 B의 배수인 자연수를 A와 B의 공배수라고 한다. 이런 공배수 중에서 가장 작은 수를 최소공배수라고 한다. 예를 들어, 6과 15의 공배수는 30, 60, 90등이 있으며, 최소 공배수는 30이다.
+
+	두 자연수 A와 B가 주어졌을 때, A와 B의 최소공배수를 구하는 프로그램을 작성하시오.
+ */
+import java.io.*;
+import java.util.*;
+public class LCM {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			bw.write(a*b/GCD(a,b)+"\n");
+		}
+		bw.flush();
+		
+	}
+	static int GCD (int a, int b) {
+		int big = a>b?a:b;
+		int small = a>b?b:a;
+		a = big%small;
+		return a==0?small:GCD(small,a);
+	}
+
+}
