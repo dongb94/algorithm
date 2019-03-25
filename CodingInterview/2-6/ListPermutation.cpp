f17a4cc70c37452f7876d01452683785ad3c88f3
DONGGEON BYEON <dongb94@gmail.com>
2019-03-25 15:02:45 +0900

    Add ListPermutation

---
+++ CodingInterview/2-6/ListPermutation.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "LinkedList.h"
+
+bool IsPermutation(linked_list<char> list) {
+	int length = 0;
+	char *buffer;
+	node<char> *pt = list.first();
+
+	while (pt != NULL) {
+		length++;
+		pt = pt->next;
+	}
+
+	buffer = new char[(length-1)/2];
+	pt = list.first();
+
+	for (int i = 0; i < (length - 1) / 2; i++) {
+		buffer[i] = pt->data;
+		pt = pt->next;
+	}
+
+	printf("%d\n", length);
+	if (length % 2 == 1) pt = pt->next;
+
+	for (int i = (length - 3) / 2; i >= 0; i--) {
+		if (buffer[i] != pt->data) return false;
+		pt = pt->next;
+	}
+
+	return true;
+}
+
+int main() {
+	linked_list<char> list;
+	list.add_node('a');
+	list.add_node('b');
+	list.add_node('c');
+	list.add_node('e');
+	list.add_node('c');
+	list.add_node('b');
+	list.add_node('a');
+
+	printf("%d\n", IsPermutation(list));
+}
