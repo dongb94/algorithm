07bd8df72be7740c5b99afe40e25860c4beaaed8
DONGGEON BYEON <dongb94@gmail.com>
2019-04-04 21:21:17 +0900

    Update Stack to template

--- Data Structure/Stack.h
+++ Data Structure/Stack.h
@@ -1,28 +1,28 @@
 #include <iostream>
 
-//template<typename T>
+template<typename T>
 class Stack {
 private:
-	int *start;
+	T *start;
 	int nOfE;
 	int size;
 public:
 
 	Stack() {
-		start = new int[10];
+		start = new T[10];
 		nOfE = 0;
 		size = 10;
 	}
 
 	Stack(int N) {
-		start = new int[N];
+		start = new T[N];
 		nOfE = 0;
 		size = N;
 	}
 
-	void push(int n) {
+	void push(T n) {
 		if (nOfE == size) {
-			int *sizeIncreaseStack = new int[size * 2];
+			T *sizeIncreaseStack = new T[size * 2];
 			sizeIncreaseStack = start;
 			for (int i = 0; i < size; i++) {
 				sizeIncreaseStack[i] = start[i];
@@ -35,11 +35,16 @@ public:
 		nOfE++;
 	}
 
-	int pop() {
+	T pop() {
 		if (nOfE == 0) return NULL;
 		nOfE--;
 
 		return start[nOfE];
+	}
+
+	T top() {
+		if (nOfE == 0) return NULL;
+		return start[nOfE - 1];
 	}
 
 	int Size() {
