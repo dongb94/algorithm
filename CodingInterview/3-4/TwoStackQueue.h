51e07ee9bcd34db3614e508486c6996276f81836
DONGGEON BYEON <dongb94@gmail.com>
2019-04-08 13:35:42 +0900

    Update TwoStackQueue.h

--- CodingInterview/3-4/TwoStackQueue.h
+++ CodingInterview/3-4/TwoStackQueue.h
@@ -5,29 +5,21 @@ class MyQueue {
 private:
 	Stack<int> in;
 	Stack<int> out;
-	bool inputFlag;
 public:
 	MyQueue() {
-		inputFlag = true;
 	}
 
 	void push(int n) {
-		if (!inputFlag) {
-			while (out.Size > 0) {
-				in.push(out.pop);
-			}
-		}
-		inputFlag = true;
 		in.push(n);
 	}
 
 	int pop() {
-		if (inputFlag) {
-			while (in.Size > 0) {
-				out.push(in.pop);
+		if (out.Size() == 0) {
+			while (in.Size() > 0) {
+				out.push(in.pop());
 			}
 		}
-		inputFlag = false;
+		if (out.Size() == 0) return NULL;
 		return out.pop();
 	}
 };
