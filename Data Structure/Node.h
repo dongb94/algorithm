5ac1c7ff365c3e48186e422f81237c18e4868d9b
DONGGEON BYEON <dongb94@gmail.com>
2019-05-17 13:43:36 +0900

    Add Node.h

---
+++ Data Structure/Node.h
@@ -0,0 +1,16 @@
+
+template <class T>
+class TreeNode{
+public:
+	T data;
+	TreeNode* parent;
+	TreeNode* left;
+	TreeNode* right;
+};
+
+template <class T>
+class ListNode {
+	T data;
+	ListNode* previous;
+	ListNode* next;
+};
