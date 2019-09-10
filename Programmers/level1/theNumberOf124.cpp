a14f05f914e08a0b25375f2315fec7c1d63a2317
DONGGEON BYEON <dongb94@gmail.com>
2019-09-10 14:58:53 +0900

    Update theNumverOf124.cpp

--- Programmers/level1/theNumberOf124.cpp
+++ Programmers/level1/theNumberOf124.cpp
@@ -1,25 +1,14 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12899
+
 #include <string>
 #include <vector>
 
 using namespace std;
 
-string to124(int n){
-    string answer = "";
-    if(n<=0) return answer;
-    switch(n%3){
-        case 0 :
-            answer = to124(n/3 - 1) + "4";
-            break;
-        case 1:
-            answer = to124(n/3) + "1";
-            break;
-        case 2:
-            answer = to124(n/3) + "2";
-            break;
-    }
-    return answer;
-}
-
 string solution(int n) {
-    return to124(n);
+    string answer = "";
+    if(n==0) return answer;
+    int rest = n%3;
+    answer = solution((n-1)/3) + "412"[rest];
+    return answer;
 }
