0786e79b51d6f5c512ba6b02c0f7e2f11ca98992
Donggeon Byeon <dongb94@gmail.com>
2022-09-28 23:00:12 +0900

    Add 새싹.cpp

---
+++ beak/level01 IO/새싹.cpp
@@ -0,0 +1,17 @@
+// https://www.acmicpc.net/problem/25083
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<< "         ,r'\"7\n" 
+        << "r`-_   ,'  ,/\n" 
+        << " \\. \". L_r'\n" 
+        << "   `~\\/\n" 
+        << "      |\n" 
+        << "      |"
+        << endl;
+
+    return 0;
+}
