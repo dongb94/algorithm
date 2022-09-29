942c6b95f771e5d0833c590527f0d4a1851ca72f
Donggeon Byeon <dongb94@gmail.com>
2022-09-29 20:29:48 +0900

    Add beakjun number 18108
    - https://www.acmicpc.net/problem/18108

---
+++ beak/level01 IO/18108.cpp
@@ -0,0 +1,14 @@
+// https://www.acmicpc.net/problem/18108
+// 1998년생인 내가 태국에서는 2541년생?!
+
+#include <iostream>
+
+int main(void)
+{
+    int inputYear;
+    std::cin >> inputYear;
+
+    std::cout << (inputYear-543);
+
+    return 0;
+}
