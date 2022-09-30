a7f56ef1dc93a905684ef10a0c8b76ea26b20132
Donggeon Byeon <dongb94@gmail.com>
2022-09-30 23:01:39 +0900

    Add beakjoon 10926, 3003

---
+++ beak/level01 IO/10926.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/10926
+// ??!
+
+#include <iostream>
+int main(void)
+{
+    char ID[50];
+    scanf("%s", ID);
+
+    printf("%s??!", ID);
+
+    return 0;
+}
---
+++ beak/level01 IO/3003.cpp
@@ -0,0 +1,21 @@
+// https://www.acmicpc.net/problem/3003
+// 킹, 퀸, 룩, 비숍, 나이트, 폰
+#include <iostream>
+
+int main(void)
+{
+    int number[6];
+    int need[] = {1,1,2,2,2,8};
+
+    for(int i=0; i<6; i++)
+    {
+        scanf("%d", &number[i]);
+    }
+    
+    for(int i=0; i<6; i++)
+    {
+        printf("%d ", need[i]-number[i]);
+    }
+
+    return 0;
+}
